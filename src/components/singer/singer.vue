2172298b48a719568b1275ef8259b5daca3c9fbc
Sakura <Sakura@163.com>
2020-01-13 23:44:43 +0800

    feat：新增歌手页面，获取数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,7 +5,35 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {getSingerList} from "api/singer";
+  import {ERR_OK} from "api/config";
+
+
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList();
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus" type="text/stylus">
+  .singer
+    opsition: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
